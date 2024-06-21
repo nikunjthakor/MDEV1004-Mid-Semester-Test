a50a40ca43ab2dc013e1bd805b10a437f59f1cf2
Nikunj <nikunjthakor2001@gmail.com>
2024-06-21 12:21:29 -0400

    New Midterm Project Added

---
+++ Server/Config/db.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let remoteURI = (process.env.Mongo_uri);
+exports.default = {
+    remoteURI: remoteURI,
+};
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,6 @@
+let remoteURI = (process.env.Mongo_uri) as string;
+
+export default {
+    remoteURI : remoteURI,
+
+}
